4f5e8ee9450a8dafeb850d00c7ffbf973d76ed4b
nagaohiroki <testplayers0226@gmail.com>
2025-09-02 23:00:26 +0900

    add UnityDAP

---
+++ UnityDAP/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace UnityDAP
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			foreach(var arg in args)
+			{
+				Console.WriteLine(arg);
+			}
+			Console.WriteLine("Hello, World!");
+		}
+	}
+}
